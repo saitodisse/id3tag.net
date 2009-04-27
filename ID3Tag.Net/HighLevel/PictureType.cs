baa9dd0b875405d403a3357bbec25dd7bf1baac3
SND\bhillebrand_cp <SND\bhillebrand_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2009-04-27 15:53:07 +0000

    Added new enum for the Picture Frame

---
+++ ID3Tag.Net/HighLevel/PictureType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ID3Tag.HighLevel
+{
+    //public enum PictureType
+    //{
+    //    Other = 0,
+    //    FileIcon_32x32,
+    //    FileIcon_Other,
+    //    Cover_Front,
+    //    Cover_Back,
+    //    LeafletPage,
+    //    Media,
+
+    //}
+
+    /*
+     * $00     Other 
+$01     32x32 pixels 'file icon' (PNG only)
+$02     Other file icon
+$03     Cover (front)
+$04     Cover (back)
+$05     Leaflet page
+$06     Media (e.g. lable side of CD)
+$07     Lead artist/lead performer/soloist
+$08     Artist/performer
+$09     Conductor
+$0A     Band/Orchestra
+$0B     Composer
+$0C     Lyricist/text writer
+$0D     Recording Location
+$0E     During recording
+$0F     During performance
+$10     Movie/video screen capture
+$11     A bright coloured fish
+$12     Illustration
+$13     Band/artist logotype
+$14     Publisher/Studio logotype
+
+     */
+}
