c312187e376e07cd20034054f3828c9524fd33be
SND\bhillebrand_cp <SND\bhillebrand_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2009-06-06 11:33:51 +0000

    Finished the ID3V1 NUnit Test.

--- ID3Tag.Net.NUnit/Highlevel/Id3V1Test.cs
+++ ID3Tag.Net.NUnit/Highlevel/Id3V1Test.cs
@@ -113,11 +113,11 @@ namespace ID3Tag.Net.NUnit.Highlevel
             var bytes = audioStream.ToArray();
 
             var tag = ReadFromStream(bytes);
-            Compare(tag.Title, '1', 30);
-            Compare(tag.Artist, '2', 30);
-            Compare(tag.Album, '3', 30);
-            Compare(tag.Year, '4', 4);
-            Compare(tag.Comment, '5', 28);
+            CheckID3Tag(tag.Title, '1', 30);
+            CheckID3Tag(tag.Artist, '2', 30);
+            CheckID3Tag(tag.Album, '3', 30);
+            CheckID3Tag(tag.Year, '4', 4);
+            CheckID3Tag(tag.Comment, '5', 28);
 
             Assert.IsTrue(tag.IsID3V1_1Compliant);
             Assert.AreEqual(tag.TrackNr, "6");
@@ -142,11 +142,11 @@ namespace ID3Tag.Net.NUnit.Highlevel
 
             var tag = ReadFromStream(bytes);
 
-            Compare(tag.Title, '1', 30);
-            Compare(tag.Artist, '2', 30);
-            Compare(tag.Album, '3', 30);
-            Compare(tag.Year, '4', 4);
-            Compare(tag.Comment, '5', 30);
+            CheckID3Tag(tag.Title, '1', 30);
+            CheckID3Tag(tag.Artist, '2', 30);
+            CheckID3Tag(tag.Album, '3', 30);
+            CheckID3Tag(tag.Year, '4', 4);
+            CheckID3Tag(tag.Comment, '5', 30);
 
             Assert.IsFalse(tag.IsID3V1_1Compliant);
             Assert.IsEmpty(tag.TrackNr);
@@ -173,7 +173,7 @@ namespace ID3Tag.Net.NUnit.Highlevel
 
             WriteToStream(audioData, output, id3Tag1);
             var id3Tag2 = ReadFromStream(output);
-            Compare(id3Tag1, id3Tag2);
+            CheckID3Tag(id3Tag1, id3Tag2);
         }
 
         [Test]
@@ -197,24 +197,181 @@ namespace ID3Tag.Net.NUnit.Highlevel
             WriteToStream(audioData, output, id3Tag1);
 
             var id3Tag2 = ReadFromStream(output);
-            Compare(id3Tag1,id3Tag2);
+            CheckID3Tag(id3Tag1,id3Tag2);
         }
 
-        /*
-         *  Testen: Schreiben von 
-         *  
-         *  - Audio Daten < 128
-         *  - Audio Daten > 128
-         *  - Tag normal
-         *  - Tag felder zu lang
-         *  - ID3v1.1  ok
-         *  - ID3v1 ok
-         */
+        [Test]
+        public void WriteWithMaxFieldSize()
+        {
+            var audioData = new byte[] { 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19 };
+            var output = new byte[138];
+
+            var id3Tag1 = new Id3V1Tag
+                              {
+                                    Title = CreateFieldText(30, '1'),
+                                    Artist = CreateFieldText(30,'2'),
+                                    Album = CreateFieldText(30,'3'),
+                                    Year = CreateFieldText(4,'4'),
+                                    Comment = CreateFieldText(28,'5'),
+                                    IsID3V1_1Compliant = true,
+                                    TrackNr = "6",
+                                    GenreIdentifier = 12
+            };
+
+            WriteToStream(audioData, output, id3Tag1);
+
+            var id3Tag2 = ReadFromStream(output);
+            CheckID3Tag(id3Tag1, id3Tag2);
+        }
+
+        [Test]
+        public void WriteWithOverMaxFieldSize_ID3v11()
+        {
+            var audioData = new byte[] { 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19 };
+            var output = new byte[138];
+
+            var id3Tag1 = new Id3V1Tag
+            {
+                Title = CreateFieldText(31, '1'),
+                Artist = CreateFieldText(31, '2'),
+                Album = CreateFieldText(31, '3'),
+                Year = CreateFieldText(5, '4'),
+                Comment = CreateFieldText(29, '5'),
+                IsID3V1_1Compliant = true,
+                TrackNr = "6",
+                GenreIdentifier = 12
+            };
+
+            WriteToStream(audioData, output, id3Tag1);
+
+            //
+            //  The coding if the fiels is over the limit. The I/O Controller cuts the string.
+            //
+            var refId3Tag = new Id3V1Tag
+            {
+                Title = CreateFieldText(30, '1'),
+                Artist = CreateFieldText(30, '2'),
+                Album = CreateFieldText(30, '3'),
+                Year = CreateFieldText(4, '4'),
+                Comment = CreateFieldText(28, '5'),
+                IsID3V1_1Compliant = true,
+                TrackNr = "6",
+                GenreIdentifier = 12
+            };
+
+            var id3Tag2 = ReadFromStream(output);
+            CheckID3Tag(refId3Tag, id3Tag2);
+        }
+
+        [Test]
+        public void WriteWithOverMaxFieldSize_ID3v1()
+        {
+            var audioData = new byte[] { 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19 };
+            var output = new byte[138];
+
+            var id3Tag1 = new Id3V1Tag
+            {
+                Title = CreateFieldText(31, '1'),
+                Artist = CreateFieldText(31, '2'),
+                Album = CreateFieldText(31, '3'),
+                Year = CreateFieldText(5, '4'),
+                Comment = CreateFieldText(31, '5'),
+                IsID3V1_1Compliant = false,
+                GenreIdentifier = 12
+            };
+
+            WriteToStream(audioData, output, id3Tag1);
+
+            //
+            //  The coding if the fiels is over the limit. The I/O Controller cuts the string.
+            //
+            var refId3Tag = new Id3V1Tag
+            {
+                Title = CreateFieldText(30, '1'),
+                Artist = CreateFieldText(30, '2'),
+                Album = CreateFieldText(30, '3'),
+                Year = CreateFieldText(4, '4'),
+                Comment = CreateFieldText(30, '5'),
+                IsID3V1_1Compliant = false,
+                GenreIdentifier = 12
+            };
+
+            var id3Tag2 = ReadFromStream(output);
+            CheckID3Tag(refId3Tag, id3Tag2);
+        }
+
+        [Test]
+        public void WriteAudioContent1()
+        {
+            var audioData = new byte[10];
+            var output = new byte[audioData.Length + 128];
+
+            //
+            // Create dummy audio content..
+            //
+            for (byte i = 0; i < audioData.Length; i++)
+            {
+                audioData[i] = i;
+            }
+
+            // Create a new tag description.
+            var id3Tag1 = new Id3V1Tag
+            {
+                Title = "1",
+                Artist = "2",
+                Album = "3",
+                Year = "4",
+                Comment = "5",
+                IsID3V1_1Compliant = false,
+                GenreIdentifier = 12
+            };
+
+            WriteToStream(audioData, output, id3Tag1);
+
+            for (byte i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(output[i], i);
+            }
+        }
+
+        [Test]
+        public void WriteAudioContent2()
+        {
+            var audioData = new byte[150];
+            var output = new byte[audioData.Length + 128];
+
+            //
+            // Create dummy audio content..
+            //
+            for (byte i = 0; i<audioData.Length;i++)
+            {
+                audioData[i] = i;
+            }
+
+            // Create a new tag description.
+            var id3Tag1 = new Id3V1Tag
+            {
+                Title = "1",
+                Artist = "2",
+                Album = "3",
+                Year = "4",
+                Comment = "5",
+                IsID3V1_1Compliant = false,
+                GenreIdentifier = 12
+            };
+
+            WriteToStream(audioData, output, id3Tag1);
+
+            for (byte i = 0; i < 150; i++)
+            {
+                Assert.AreEqual(output[i], i);
+            }
+        }
 
         #region Helper...
 
 
-        private static void Compare(string value, char refChar, int count)
+        private static void CheckID3Tag(string value, char refChar, int count)
         {
             var refValue = new StringBuilder();
             for (int i=0; i<count; i++)
@@ -222,10 +379,10 @@ namespace ID3Tag.Net.NUnit.Highlevel
                 refValue.Append(refChar);
             }
 
-            Assert.AreEqual(value,refValue.ToString(),"Compare operation failed : " + value);
+            Assert.AreEqual(value,refValue.ToString(),"CheckID3Tag operation failed : " + value);
         }
 
-        private static void Compare(Id3V1Tag tag1, Id3V1Tag tag2)
+        private static void CheckID3Tag(Id3V1Tag tag1, Id3V1Tag tag2)
         {
             Assert.AreEqual(tag1.Album,tag2.Album);
             Assert.AreEqual(tag1.Artist,tag2.Artist);
@@ -256,6 +413,18 @@ namespace ID3Tag.Net.NUnit.Highlevel
             }
 
             return bytes;
+        }
+
+        private static string CreateFieldText(int count, char c)
+        {
+            var sb = new StringBuilder();
+
+            for (var counter = 0; counter < count; counter++)
+            {
+                sb.Append(c);
+            }
+
+            return sb.ToString();
         }
 
         private static void WriteToStream(byte[] audioData, byte[] output, Id3V1Tag id3Tag)
