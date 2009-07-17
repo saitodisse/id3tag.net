4c8e9deb4c0e1e24e3afe6b30a41c0c515e0b70a
SND\bhillebrand_cp <SND\bhillebrand_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2009-07-17 16:02:23 +0000

    Add missing file

---
+++ R0.6/ID3Tag.Net.NUnit/Lowlevel/FramesTestV4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ID3Tag.Factory;
+using NUnit.Framework;
+
+namespace ID3Tag.Net.NUnit.Lowlevel
+{
+    [TestFixture]
+    public class FramesTestV4 : Test
+    {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void Setup()
+        {
+            m_Controller = Id3TagFactory.CreateIoController();
+        }
+
+        #endregion
+
+        // TODO:
+    }
+}
